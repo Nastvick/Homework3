3eb246df3b0bdd65a218d66f1b603ce1171fd822
root <root@DESKTOP-LVGK8JP.localdomain>
2021-10-12 18:49:37 +0300

    Deck generator

--- homework8_deck.py
+++ homework8_deck.py
@@ -1,7 +1,19 @@
-def my_deck(func, suits):
+def my_deck(*suits):
     for i in suits:
-        yield func(i)
+        yield i
 
-for i in my_deck(, range(4)):
+def my_deck(*values):
+    for i in values:
+        yield i
+
+for i in my_deck("Hearts", "Diamonds", "Clubs", "Spades"):
     print(i)
+
+for i in my_deck('A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'):
+    print(i)
+
+
+
+
+
 
