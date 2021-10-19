57d7c9abd54a884b5aa721a536e42de6e7c7a5e1
root <root@DESKTOP-LVGK8JP.localdomain>
2021-10-19 18:58:44 +0300

    Decorators

---
+++ Lecture9/decorators.py
@@ -0,0 +1,14 @@
+def deco_adds_2(f):
+    def inner(a, b):
+        res = (a+b)+2
+        print('AFTER WITH RESULT', res)
+        return res
+    return inner
+
+@deco_adds_2
+def add_sum(a, b):
+     return a + b
+
+
+print(add_sum(10, 10))  # >>> 22
+print(add_sum(2, 3))  # >>> 7
